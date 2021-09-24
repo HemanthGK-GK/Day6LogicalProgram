96db462ebb98afe9c3f78afe3c01dc1ce21a766a
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 22:57:17 +0530

    Hemanth : Reverse the given number

---
+++ ReverseNum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int remainder = 0;
+            int reverse = 0;
+            Console.WriteLine("Enter Number");
+            int num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            { 
+            
+            remainder = num % 10;
+            reverse = reverse * 10 + remainder;
+            num = num / 10;
+            }
+            Console.WriteLine("Reverse Number is :"+reverse);
+
+
+        }
+    }
+}
