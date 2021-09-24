63f7efc7df470080f2f6f3fcb139a8ab8d723d06
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 23:04:07 +0530

    Hemanth : Stopwatch Program for measuring the time that elapses between the start and end clicks

---
+++ StopWatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics; 
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopw = new Stopwatch();
+            stopw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(2);
+            }
+            stopw.Stop();
+            Console.WriteLine(" Time elapsed:  "+ stopw.Elapsed);
+        }
+    }
+}
+    
