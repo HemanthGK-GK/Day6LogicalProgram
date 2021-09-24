6d9b413229467b4dd59762da296c7b713bdef848
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 22:53:25 +0530

    Hemanth : Find the Number is Perfect Number or not

---
+++ PerfectNum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PerfectNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0,i=1;
+            Console.WriteLine("Enter The Number to check It is a Perfect num of not");
+            num = int.Parse(Console.ReadLine());
+
+            while(i<=num/2)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+                i++;
+            }
+            if(sum==num)
+            {
+                Console.WriteLine(num + " Is a Perfect Number");
+            }
+            else
+                Console.WriteLine(num + " Is Not a Perfect Number");
+        }
+    }
+}
